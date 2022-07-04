24a9cf63e0018e0a8b98fb6bc464f8c7949f02b5
Lucas Dias <lucas.dias1987lu@gmail.com>
2022-07-04 20:24:42 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,29 +1,50 @@
 function calculaIMC(){
     const massa = document.getElementById("massa").value;  
     const altura = document.getElementById("altura").value;  
+    
    
     console.log("Massa: "+massa);  
     console.log("Altura: "+altura);  
     const  imc = massa/(altura**2)
-    document.getElementById("resultado").innerHTML = "Resultado: "+imc 
+
+    
+    
+    document.getElementById("resultado").innerHTML = "Resultado: "+imc; 
+    
     if(imc<18.5){
-        document.getElementById("resultado").innerHTML = "Resultado: "+imc + " - Abaixo do peso"
+        document.getElementById("resultado").innerHTML = "Resultado: "+imc.toFixed(2) + " - Abaixo do peso"
+       resultado.style.color = "red"
+       resultado.style.backgroundColor = "white"
+       esultado.style.fontSize = "40px"
     }
     if(imc>=18.5 && imc<25){
-        document.getElementById("resultado").innerHTML = "Resultado: "+imc + " - Peso ideal"
-        
+        document.getElementById("resultado").innerHTML = "Resultado: "+imc.toFixed(2) + " - Peso ideal"
+        resultado.style.color = "green"
+        resultado.style.backgroundColor = "white"
     }
     if(imc>=25 && imc<30){
-        document.getElementById("resultado").innerHTML = "Resultado: "+imc + " - Sobrepeso"
+        document.getElementById("resultado").innerHTML = "Resultado: "+imc.toFixed(2) + " - Sobrepeso"
+        resultado.style.color = "orange"
+        resultado.style.backgroundColor = "white"
+        resultado.style.fontSize = "32px"
     }
     if(imc>=30 && imc<35){
-        document.getElementById("resultado").innerHTML = "Resultado: "+imc + " - Obesidade grau 1"
+        document.getElementById("resultado").innerHTML = "Resultado: "+imc.toFixed(2) + " - Obesidade grau 1"
+        resultado.style.color = "red"
+        resultado.style.backgroundColor = "white"
+        esultado.style.fontSize = "33px"
     }   
     if(imc>=35 && imc<40){  
-        document.getElementById("resultado").innerHTML = "Resultado: "+imc + " - Obesidade grau 2"
+        document.getElementById("resultado").innerHTML = "Resultado: "+imc.toFixed(2) + " - Obesidade grau 2"
+        resultado.style.color = "red"
+        resultado.style.backgroundColor = "white"
+        esultado.style.fontSize = "34px"
     }
     if(imc>=40){
-        document.getElementById("resultado").innerHTML = "Resultado: "+imc + " - Obesidade grau 3"
+        document.getElementById("resultado").innerHTML = "Resultado: "+imc.toFixed(2) + " - Obesidade grau 3"
+        resultado.style.color = "red"
+        resultado.style.backgroundColor = "white"
+        esultado.style.fontSize = "35px"
     }
     
 }
